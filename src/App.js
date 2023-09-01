6623356a289871cc5fc10879520be2c2fec3ccae
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-09-02 00:26:35 +0300

    testing useState (done)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
+
 function App() {
+
+  const [numbers, setNumbers] = useState([1, 2, 4])
+
+  const addNumber = () => {
+    const randNumber = Math.round(Math.random() * 10)
+    const newArr = [...numbers, randNumber]
+    setNumbers(newArr)
+  }
+
   return (
     <div className="App">
-      Hi
+      <button onClick={addNumber}>ADD</button>
+      <ul>
+        {numbers.map((num, index) => (
+          <li key={index}>{num}</li>
+        ))}
+      </ul>
+      
     </div>
   );
 }
