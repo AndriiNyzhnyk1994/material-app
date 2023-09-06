7cca9424b69cb43c7b3c63d3a9f9402e140f2e11
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-09-06 20:16:58 +0300

    useCallback testing (part 2 done)

--- src/Lesson-4/Lesson4.jsx
+++ src/Lesson-4/Lesson4.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useRef, useState } from 'react'
 
 export function Lesson4() {
-    
+
     const [numbers, setNumbers] = useState([1, 2, 3, 4])
     const ulRef = useRef()
     const numbersRef = useRef()
@@ -9,7 +9,7 @@ export function Lesson4() {
     numbersRef.current = numbers
 
     const addNumber = () => {
-        setNumbers( prev => [...prev, prev[prev.length - 1] + 1])
+        setNumbers(prev => [...prev, prev[prev.length - 1] + 1])
     }
 
     const handleScroll = useCallback(() => {
@@ -25,9 +25,20 @@ export function Lesson4() {
     }
 
 
+    // -----------------------  Example 2 -------------------------
+
+    const [count, setCount] = useState(0)
+
+    const onPlus = useCallback(() => setCount(count => count + 1), [])
+    const onMinus = useCallback(() => setCount(count => count - 1), [])
+
+
+
+
     return (
         <div>
             <h2>useCallback</h2>
+            <h4>Part 1</h4>
             <button onClick={addNumber}>add item</button>
             <ul ref={ulRef} style={{ overflow: 'scroll', height: '200px' }} >
                 {numbers.map((n) => {
@@ -38,6 +49,34 @@ export function Lesson4() {
             </ul>
             <button onClick={start}>START</button>
             <button onClick={stop}>STOP</button>
+
+            <h4>Part 1</h4>
+            <h1>{count}</h1>
+            <Buttons onPlus={onPlus} onMinus={onMinus} />
         </div>
     )
 }
+
+
+const Buttons = React.memo(
+    ({ onPlus, onMinus }) => {
+        console.log('buttons render')
+
+        return (
+            <>
+                <button onClick={onPlus}>+</button>
+                <button onClick={onMinus}>-</button>
+            </>
+        )
+
+    })
+
+
+
+
+
+
+
+
+
+
